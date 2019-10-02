ab5e6bf246ee55756cb5d71532803f4cbbb8b607
= <craig@craigshoemaker.net>
2019-10-02 15:36:12 -0400

    Implement passive processing moved logic to background.js no longer requires explicit button click

--- src/background.js
+++ src/background.js
@@ -1,6 +1,17 @@
 'use strict';
 
+function sendMessage(message) {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    const id = tabs[0].id;
+    chrome.tabs.sendMessage(id, message);
+  });
+}
+
 chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    sendMessage({ action: 'load' });
+  });
+
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     chrome.declarativeContent.onPageChanged.addRules([
       {
--- src/content.js
+++ src/content.js
@@ -16,38 +16,21 @@ function getEditUrl(url) {
   return `${url}?message=[PARTNER%20EDIT]:%20&description=%3C%21--%20Please%20include%20%5BPARTNER%20EDIT%5D%20in%20your%20commit%20and%20PR%20title%20--%3E`;
 }
 
-function openUrl(url) {
-  const link = document.createElement('a');
-  link.href = url;
-  link.target = '_blank';
-
-  document.body.appendChild(link);
-
-  link.click();
-  link.remove();
-}
-
-function run(document) {
-  let url = getUrl(document);
-
-  if (url.length > 0) {
-    url = getEditUrl(url);
-    openUrl(url);
-  } else {
-    chrome.runtime.sendMessage({ action: 'noUrl' });
-  }
-}
-
 function load() {
   if (/https?\:\/\/docs\.microsoft\.com/.test(window.location.href)) {
-    chrome.runtime.sendMessage({ action: 'enable' });
+    let el = document.querySelector(`a[${URL_ATTRIBUTE}]`);
+
+    if (el) {
+      let url = getUrl(document);
+      url = getEditUrl(url);
+      el.setAttribute('href', url);
+      el.setAttribute('target', '_blank');
+    }
   }
 }
 
 chrome.runtime.onMessage.addListener(request => {
   if (request.action === 'load') {
     load();
-  } else if (request.action === 'run') {
-    run(document);
   }
 });
