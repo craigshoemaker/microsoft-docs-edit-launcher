062c4700b1d2fa1d0bb49e6b9c32079e76fc00a7
= <craig@craigshoemaker.net>
2019-10-03 10:00:07 -0400

    Refactoring

--- src/content.js
+++ src/content.js
@@ -1,30 +1,31 @@
 const URL_ATTRIBUTE = 'data-original_content_git_url';
 
-function getUrl(document) {
-  let url = '';
-  const anchor = document.querySelector(`a[${URL_ATTRIBUTE}]`);
+let editAnchor = null;
 
+function getUrl(anchor) {
+  let url = '';
   if (anchor) {
     url = anchor.getAttribute(URL_ATTRIBUTE);
   }
-
   return url;
 }
 
 function getEditUrl(url) {
-  url = url.replace('/blob/', '/edit/');
-  return `${url}?message=[PARTNER%20EDIT]:%20&description=%3C%21--%20Please%20include%20%5BPARTNER%20EDIT%5D%20in%20your%20commit%20and%20PR%20title%20--%3E`;
+  if (/\/blob\//.test(url)) {
+    url = url.replace('/blob/', '/edit/');
+    url = `${url}?message=[PARTNER%20EDIT]:%20&description=%3C%21--%20Please%20include%20%5BPARTNER%20EDIT%5D%20in%20your%20commit%20and%20PR%20title%20--%3E`;
+  }
+  return url;
 }
 
 function load() {
   if (/https?\:\/\/docs\.microsoft\.com/.test(window.location.href)) {
-    let el = document.querySelector(`a[${URL_ATTRIBUTE}]`);
-
-    if (el) {
-      let url = getUrl(document);
+    if (!editAnchor) {
+      editAnchor = document.querySelector(`a[${URL_ATTRIBUTE}]`);
+      let url = getUrl(editAnchor);
       url = getEditUrl(url);
-      el.setAttribute('href', url);
-      el.setAttribute('target', '_blank');
+      editAnchor.setAttribute('href', url);
+      editAnchor.setAttribute('target', '_blank');
     }
   }
 }
