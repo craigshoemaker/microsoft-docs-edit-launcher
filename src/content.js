7d89be75e4f45ccaea36c39ed063f6ba490c5739
= <craig@craigshoemaker.net>
2019-10-05 09:34:10 -0400

    Refactored code - made domain support extensible - removed need for many if statements - removed need for null checks

--- src/content.js
+++ src/content.js
@@ -1,52 +1,45 @@
-const URL_ATTRIBUTE = 'data-original_content_git_url';
+'use strict';
 
-let editAnchor = null;
+const getAuthor = () => document.querySelector('meta[name="author"]').getAttribute('content');
 
-const yes = 0;
-
-function getUrl(anchor) {
-  let url = '';
-  if (anchor) {
-    url = anchor.getAttribute(URL_ATTRIBUTE);
+const domains = {
+  'docs.microsoft.com': {
+    selector: `a[data-original_content_git_url]`,
+    attribute: 'data-original_content_git_url'
   }
-  return url;
-}
+};
 
-function getAuthor() {
-  const meta = document.querySelector('meta[name="author"]')
-  return meta.getAttribute('content');
-}
-
-function getEditUrl(url, author) {
-  if (/\/blob\//.test(url)) {
-    url = url.replace('/blob/', '/edit/');
-    url = url.replace('/live/', '/master/');
-    url = `${url}?message=[PARTNER%20EDIT]:%20&description=%0A%0Acc%3A%20%40${author}%0A%0A%3C%21--%20Please%20include%20%5BPARTNER%20EDIT%5D%20in%20your%20commit%20and%20PR%20title%20--%3E`;
+const strategy = {
+  rules: [
+    { apply: url => url.replace('/blob/', '/edit/') },
+    { apply: url => url.replace('/live/', '/master/') },
+  ],
+  run: (domain) => {
+    const author = getAuthor();
+    const anchors = document.querySelectorAll(domain.selector);
+    [].forEach.call(anchors, a => {
+      let url = a.getAttribute(domain.attribute);
+      strategy.rules.forEach(rule => {
+        url = rule.apply(url);
+      });
+      url = `${url}?message=[PARTNER%20EDIT]:%20&description=%0A%0Acc%3A%20%40${author}%0A%0A%3C%21--%20Please%20include%20%5BPARTNER%20EDIT%5D%20in%20your%20commit%20and%20PR%20title%20--%3E`;
+      a.setAttribute('href', url);
+      a.setAttribute('target', '_blank');
+    });
   }
-  return url;
-}
+};
 
-function setAnchorAttributes() {
-  let url = getUrl(editAnchor);
-  const author = getAuthor();
-  url = getEditUrl(url, author);
-  editAnchor.setAttribute('href', url);
-  editAnchor.setAttribute('target', '_blank');
-}
-
-function load() {
-  if (/https?\:\/\/docs\.microsoft\.com/.test(window.location.href)) {
-    if (!editAnchor) {
-      editAnchor = document.querySelector(`a[${URL_ATTRIBUTE}]`);
-      if(editAnchor) {
-        setAnchorAttributes();
-      }
+const actions = {
+  load: () => {
+    const domain = domains[window.location.host];
+    if (domain) {
+      strategy.run(domain);
     }
   }
 }
 
 chrome.runtime.onMessage.addListener(request => {
-  if (request.action === 'load') {
-    load();
+  if(actions[request.action]) {
+    actions[request.action]();
   }
 });
