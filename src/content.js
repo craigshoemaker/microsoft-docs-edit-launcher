ae7ac7c76667413b94def5ba9aed46be1694b6ce
= <craig@craigshoemaker.net>
2019-10-06 07:27:31 -0400

    Move rules into domain object

--- src/content.js
+++ src/content.js
@@ -12,28 +12,41 @@ const getAuthor = () => {
 const domains = {
   'docs.microsoft.com': {
     selector: `a[data-original_content_git_url]`,
-    attribute: 'data-original_content_git_url'
-  }
+    attribute: 'data-original_content_git_url',
+    isMatch: () => true,
+    rules: [
+      { apply: url => url.replace('/blob/', '/edit/') },
+      { apply: url => url.replace('/live/', '/master/') },
+      { apply: url => `${url}?message=[PARTNER%20EDIT]:%20` },
+      { apply: url => `${url}&description=` },
+      {
+        apply: url => {
+          let author = getAuthor();
+          if (author.length > 0) {
+            author = `%0A%0Acc%3A%20%40${author}`;
+          }
+          return `${url}${author}`;
+        },
+      },
+      { apply: url => `${url}%0A%0A%3C%21--%20Please%20include%20%5BPARTNER%20EDIT%5D%20in%20your%20commit%20and%20PR%20title%20--%3E` },
+    ],
+  },
 };
 
 const strategy = {
-  rules: [
-    { apply: url => url.replace('/blob/', '/edit/') },
-    { apply: url => url.replace('/live/', '/master/') },
-  ],
-  run: (domain) => {
-    const author = getAuthor();
-    const anchors = document.querySelectorAll(domain.selector);
-    [].forEach.call(anchors, a => {
-      let url = a.getAttribute(domain.attribute);
-      strategy.rules.forEach(rule => {
-        url = rule.apply(url);
+  run: domain => {
+    if (domain.isMatch()) {
+      const anchors = document.querySelectorAll(domain.selector);
+      [].forEach.call(anchors, a => {
+        let url = a.getAttribute(domain.attribute);
+        domain.rules.forEach(rule => {
+          url = rule.apply(url);
+        });
+        a.setAttribute('href', url);
+        a.setAttribute('target', '_blank');
       });
-      url = `${url}?message=[PARTNER%20EDIT]:%20&description=%0A%0Acc%3A%20%40${author}%0A%0A%3C%21--%20Please%20include%20%5BPARTNER%20EDIT%5D%20in%20your%20commit%20and%20PR%20title%20--%3E`;
-      a.setAttribute('href', url);
-      a.setAttribute('target', '_blank');
-    });
-  }
+    }
+  },
 };
 
 const actions = {
